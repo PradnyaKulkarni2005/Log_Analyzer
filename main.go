37ca8f3e360c12e898be2073618553d3193a58e5
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2026-03-24 09:23:19 +0530

    added goroutines to count error

--- main.go
+++ main.go
@@ -4,7 +4,21 @@ import (
 	"fmt" // for printing output
 	"os" // work with files - open, close
 	"strings" // for string manipulation
+	
 )
+// jobs channnel - read only channel to send log lines to worker goroutines
+// results channel - write only channel to receive counts from worker goroutines
+// worker - receives line , processes it and sends results back
+func worker(jobs <-chan string , results chan<- int){
+	for line := range jobs {
+		count :=0
+		if isErrorLine(line) {
+			count =1
+		}
+	results <-count
+}
+}
+
 func isErrorLine(line string) bool {
 	return strings.Contains(line, "ERROR") // check if the line contains the word "ERROR"
 }
@@ -33,27 +47,32 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	// this will create a scanner object that will read the file line by line i.e doesnt load the entire file into memory at once
 	errorCount := 0
-	infoCount := 0
-	ipCount := make(map[string]int) // create a map to count the occurrences of each IP address
+	// infoCount := 0
+	// ipCount := make(map[string]int) // create a map to count the occurrences of each IP address
+	
+	jobs := make(chan string) // create a channel to send lines to worker goroutines , it will carry log lines
 
-	for scanner.Scan(){
-		line :=scanner.Text()
-		if isErrorLine(line) {
-			errorCount++
-		}
-		if isInfoLine(line) {
-			infoCount++
-		}
-		if ip,ok := extractIP(line); ok {
-			ipCount[ip]++ // increment the count for this IP address
-		}
+	results := make(chan int) // create a channel to receive counts from worker goroutines , it will carry counts of errors
+	numWorkers :=3
 
+	for i:=0; i<numWorkers; i++{
+		go worker(jobs, results)//starts a gouroutine that runs the worker function, passing the jobs and results channels as arguments
+	}
+
+	go func(){
+		for scanner.Scan() {
+			jobs <- scanner.Text() //sends lines to workers
+		}
+		close(jobs)
+	}()
+	for i:=0;i<4;i++{
+		errorCount += <-results // receive counts from workers and add them to the total error count
 	}
 	fmt.Printf("Total number of errors: %d\n", errorCount) // print the total number of errors
-	fmt.Printf("Total number of info messages: %d\n", infoCount) // print the total number of info messages
+	// fmt.Printf("Total number of info messages: %d\n", infoCount) // print the total number of info messages
 
-	fmt.Println("IP address counts:") // print the header for the IP address counts
-	for ip,count :=range ipCount {
-		fmt.Println(ip, ":", count) 
-	}
+	// fmt.Println("IP address counts:") // print the header for the IP address counts
+	// for ip,count :=range ipCount {
+	// 	fmt.Println(ip, ":", count) 
+	// }
 }
