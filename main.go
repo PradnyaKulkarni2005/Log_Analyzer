a13d795d9dd2fcee812f07755e7854f5f4c58c4e
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2026-03-25 22:58:20 +0530

    added flags to improve the output

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main // entry point for the program
 import (
 	"bufio" // for reading files
-	"fmt" // for printing output
-	"os" // work with files - open, close
+	"flag"  // for command-line flag parsing
+	"fmt"   // for printing output
+	"os"    // work with files - open, close
 	"strings" // for string manipulation
-	"sync" // for synchronization primitives like WaitGroup
+	"sync"    // for synchronization primitives like WaitGroup
 )
 
 type Result struct {
@@ -50,10 +51,12 @@ func extractIP(line string) (string,bool){
 	return "", false // return an empty string and false to indicate that no IP was found
 }
 func main() {
-	file, err := os.Open("log.txt") // open the file
-	// returns a file object and an error object
-	// go doesnt use exceptions, instead it returns an error object that we can check
-	// if error comes then error handling
+	filepath := flag.String("file","log.txt","path to log file")
+	showErrors := flag.Bool("errors", true, "show error count")
+	showInfo := flag.Bool("info", true, "show info count")
+	showIP := flag.Bool("ip", true, "show IP stats")
+	flag.Parse() // parse the command-line flags
+	file, err := os.Open(*filepath) // open the log file specified by the user
 
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -67,9 +70,9 @@ func main() {
 	ipCount := make(map[string]int) // create a map to count the occurrences of each IP address
 	
 	jobs := make(chan string) // create a channel to send lines to worker goroutines , it will carry log lines
-
 	results := make(chan Result) // create a channel to receive counts from worker goroutines 
 	numWorkers :=3
+	
 	var wg sync.WaitGroup
 
 	for i:=0; i<numWorkers; i++{
@@ -95,11 +98,19 @@ func main() {
 			ipCount[result.ip]++ // increment the count for this IP address in the map
 		} // add the count from the worker to the total error count
 	}
-	fmt.Printf("Total number of errors: %d\n", errorCount) // print the total number of errors
-	fmt.Printf("Total number of info messages: %d\n", infoCount) // print the total number of info messages
+	
+	if *showErrors {
+	fmt.Println("Errors:", errorCount)
+}
 
-	fmt.Println("IP address counts:") // print the header for the IP address counts
-	for ip,count :=range ipCount {
-		fmt.Println(ip, ":", count) 
+if *showInfo {
+	fmt.Println("Info:", infoCount)
+}
+
+if *showIP {
+	fmt.Println("IP Counts:")
+	for ip, count := range ipCount {
+		fmt.Println(ip, ":", count)
 	}
+}
 }
